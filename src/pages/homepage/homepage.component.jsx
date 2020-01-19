9f75d59a77aa107c2a8cbc73458203a61250e5e1
Nikhil Mehral <nikhil.mehral@hotmail.com>
2020-01-19 12:14:54 +1000

    Added router withRouter api

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component'
 
-export const HomePage = () => (
+export const HomePage = ({ history }) => (
 	<div className="homepage">
-		<Directory />
+		<Directory history={ history }/>
 	</div>
 )
