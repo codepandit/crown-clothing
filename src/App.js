935cea2dcd3f18f938b96e3aee730f07b22e2ddf
Nikhil Mehral <nikhil.mehral@hotmail.com>
2020-01-19 18:39:42 +1000

    finished sign up page with reusable components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import { HomePage } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 const HatsPage = () => (
@@ -22,6 +23,7 @@ function App() {
         <Route exact path='/' component={ HomePage }/>
         <Route exact path='/shop' component={ ShopPage }/>
         <Route path='/shop/hats' component={ HatsPage }/>
+        <Route path='/signin' component={ SignInAndSignUpPage }/>
       </Switch>
     </div>
   );
