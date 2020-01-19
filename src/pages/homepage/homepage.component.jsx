0726f4c32643ceb5ea1ab5abeefefe5ad80dd50c
Nikhil Mehral <nikhil.mehral@hotmail.com>
2020-01-19 16:21:34 +1000

    updates app with shop page, collection preview and collection item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component'
 
-export const HomePage = ({ history }) => (
+export const HomePage = () => (
 	<div className="homepage">
-		<Directory history={ history }/>
+		<Directory />
 	</div>
 )
